cfd5851e7f7617fb76d9397a52cc6eadf351b68a
Sonu Munda <sonumunda1312@gmail.com>
2023-06-29 19:58:53 +0530

    Classes Page added

---
+++ src/assets/api/ClassesList.js
@@ -0,0 +1,319 @@
+const ClassesList = [
+  {
+    day: "Monday",
+    classes: [
+      {
+        time: "6.00AM - 8.00AM",
+        name: "Muscle Building",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "8.00AM - 10.00AM",
+        name: "Power Lifting",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "10.00AM - 12.00PM",
+        name: "Crossfit Class",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "12.00PM-2.00PM",
+        name: "Cardio Program",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "2.00PM - 4.00PM",
+        name: "Weight Loss",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "4.00PM - 6.00PM",
+        name: "Yoga Class",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "6.00PM - 8.00PM",
+        name: "Fitness Program",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "8.00PM - 10.00PM",
+        name: "Body Building",
+        instructor: "Mahesh Dubey",
+      },
+    ],
+  },
+  {
+    day: "Tuesday",
+    classes: [
+      {
+        time: "6.00AM - 8.00AM",
+        name: "Crossfit Class",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "8.00AM - 10.00AM",
+        name: "Muscle Building",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "10.00AM - 12.00PM",
+        name: "Cardio Program",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "12.00PM-2.00PM",
+        name: "Power Lifting",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "2.00PM - 4.00PM",
+        name: "Body Building",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "4.00PM - 6.00PM",
+        name: "Yoga Class",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "6.00PM - 8.00PM",
+        name: "Weight Loss",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "8.00PM - 10.00PM",
+        name: "Fitness Program",
+        instructor: "Mohit Kaushal",
+      },
+    ],
+  },
+  {
+    day: "Wednesday",
+    classes: [
+      {
+        time: "6.00AM - 8.00AM",
+        name: "Fitness Program",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "8.00AM - 10.00AM",
+        name: "Muscle Building",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "10.00AM - 12.00PM",
+        name: "Weight Loss",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "12.      .00PM-2.00PM",
+        name: "Power Lifting",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "2.00PM - 4.00PM",
+        name: "Crossfit Class",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "4.00PM - 6.00PM",
+        name: "Cardio Program",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "6.00PM - 8.00PM",
+        name: "Body Building",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "8.00PM - 10.00PM",
+        name: "Yoga Class",
+        instructor: "Anushk Gorak",
+      },
+    ],
+  },
+  {
+    day: "Thursday",
+    classes: [
+      {
+        time: "6.00AM - 8.00AM",
+        name: "Crossfit Class",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "8.00AM - 10.00AM",
+        name: "Cardio Program",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "10.00AM - 12.00PM",
+        name: "Fitness Program",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "12.00PM-2.00PM",
+        name: "Power Lifting",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "2.00PM - 4.00PM",
+        name: "Muscle Building",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "4.00PM - 6.00PM",
+        name: "Body Building",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "6.00PM - 8.00PM",
+        name: "Yoga Class",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "8.00PM - 10.00PM",
+        name: "Weight Loss",
+        instructor: "Anushk Gorak",
+      },
+    ],
+  },
+  {
+    day: "Friday",
+    classes: [
+      {
+        time: "6.00AM - 8.00AM",
+        name: "Fitness Program",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "8.00AM - 10.00AM",
+        name: "Muscle Building",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "10.00AM - 12.00PM",
+        name: "Power Lifting",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "12.00PM-2.00PM",
+        name: "Crossfit Class",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "2.00PM - 4.00PM",
+        name: "Cardio Program",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "4.00PM - 6.00PM",
+        name: "Yoga Class",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "6.00PM - 8.00PM",
+        name: "Weight Loss",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "8.00PM - 10.00PM",
+        name: "Body Building",
+        instructor: "Mahesh Dubey",
+      },
+    ],
+  },
+  {
+    day: "Saturday",
+    classes: [
+      {
+        time: "6.00AM - 8.00AM",
+        name: "Weight Loss",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "8.00AM - 10.00AM",
+        name: "Crossfit Class",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "10.00AM - 12.00PM",
+        name: "Muscle Building",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "12.00PM-2.00PM",
+        name: "Cardio Program",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "2.00PM - 4.00PM",
+        name: "Power Lifting",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "4.00PM - 6.00PM",
+        name: "Yoga Class",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "6.00PM - 8.00PM",
+        name: "Fitness Program",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "8.00PM - 10.00PM",
+        name: "Body Building",
+        instructor: "Mahesh Dubey",
+      },
+    ],
+  },
+  {
+    day: "Sunday",
+    classes: [
+      {
+        time: "6.00AM - 8.00AM",
+        name: "Yoga Class",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "8.00AM - 10.00AM",
+        name: "Fitness Program",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "10.00AM - 12.00PM",
+        name: "Weight Loss",
+        instructor: "Anushk Gorak",
+      },
+      {
+        time: "12.00PM-2.00PM",
+        name: "Muscle Building",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "2.00PM - 4.00PM",
+        name: "Power Lifting",
+        instructor: "Mahesh Dubey",
+      },
+      {
+        time: "4.00PM - 6.00PM",
+        name: "Cardio Program",
+        instructor: "Kuldeep Singh",
+      },
+      {
+        time: "6.00PM - 8.00PM",
+        name: "Crossfit Class",
+        instructor: "Mohit Kaushal",
+      },
+      {
+        time: "8.00PM - 10.00PM",
+        name: "Body Building",
+        instructor: "Mahesh Dubey",
+      },
+    ],
+  },
+];
+
+export default ClassesList;
--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,9 +1,123 @@
-import "./Classes.css"
+import "./Classes.css";
+import { useState } from "react";
+import ClassesList from "../../assets/api/ClassesList";
 
 const Classes = () => {
-  return (
-    <div>Classes</div>
-  )
-}
+  const [selectedDay, setSelectedDay] = useState("Monday");
 
-export default Classes
+  const handleDayClick = (day) => {
+    setSelectedDay(day);
+  };
+
+  return (
+    <div className="classes-wrapper">
+      <section className="banner classes-banner">
+        <div className="container center">
+          <div className="banner-heading">
+            <h1>Classes</h1>
+          </div>
+        </div>
+      </section>
+
+      {/* Class Section */}
+      <section className="class-timings center py-5 my-5">
+        <div className="container center flex-col">
+          <div className="title">
+            <h4
+              className="text-lg uppercase font-bold"
+              style={{ color: "orangered" }}
+            >
+              Classes Schedule
+            </h4>
+          </div>
+          <div className="heading text-4xl uppercase font-bold mb-4 p-1">
+            Working Hours
+          </div>
+
+          <nav className="weekbar rounded mb-5">
+            <ul className="week-list center flex-wrap">
+              <li
+                className={`list-item mx-1 my-2${
+                  selectedDay === "Monday" ? "active" : ""
+                }`}
+                onClick={() => handleDayClick("Monday")}
+              >
+                Monday
+              </li>
+              <li
+                className={`list-item mx-1 ${
+                  selectedDay === "Tuesday" ? "active" : ""
+                }`}
+                onClick={() => handleDayClick("Tuesday")}
+              >
+                Tuesday
+              </li>
+              <li
+                className={`list-item mx-1 ${
+                  selectedDay === "Wednesday" ? "active" : ""
+                }`}
+                onClick={() => handleDayClick("Wednesday")}
+              >
+                Wednesday
+              </li>
+              <li
+                className={`list-item mx-1 ${
+                  selectedDay === "Thursday" ? "active" : ""
+                }`}
+                onClick={() => handleDayClick("Thursday")}
+              >
+                Thursday
+              </li>
+              <li
+                className={`list-item mx-1 ${
+                  selectedDay === "Friday" ? "active" : ""
+                }`}
+                onClick={() => handleDayClick("Friday")}
+              >
+                Friday
+              </li>
+              <li
+                className={`list-item mx-1 ${
+                  selectedDay === "Saturday" ? "active" : ""
+                }`}
+                onClick={() => handleDayClick("Saturday")}
+              >
+                Saturday
+              </li>
+              <li
+                className={`list-item mx-1 ${
+                  selectedDay === "Sunday" ? "active" : ""
+                }`}
+                onClick={() => handleDayClick("Sunday")}
+              >
+                Sunday
+              </li>
+            </ul>
+          </nav>
+
+          {selectedDay && (
+            <div className="time-table center flex-wrap">
+              {ClassesList.find((day) => day.day === selectedDay)?.classes.map(
+                (classInfo) => (
+                  <div className= "class-col center flex-col" key={classInfo.name}>
+                    <div className="class-timings">
+                      <p className="text-white uppercase font-bold">{classInfo.time}</p>
+                    </div>
+                    <div className="class-name">
+                      <h3 className="font-bold uppercase text-2xl p-3" style={{color:"orangered"}}>{classInfo.name}</h3>
+                    </div>
+                    <div className="class-instructor">
+                      <p className="text-white uppercase">{classInfo.instructor}</p>
+                    </div>
+                  </div>
+                )
+              )}
+            </div>
+          )}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Classes;
