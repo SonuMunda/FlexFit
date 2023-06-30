93d461598604b954b9576967d7c1d410fbdc0313
Sonu Munda <sonumunda1312@gmail.com>
2023-06-30 16:53:11 +0530

    Navbar Responsive

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,38 +1,87 @@
 import "./Header.css";
+import { FaBars } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 const Header = () => {
+  const [navActive, setNavActive] = useState(false);
+  const toggleNav = () => {
+    if (navActive === false) {
+      setNavActive(true);
+    } else {
+      setNavActive(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
   return (
     <header className="header center">
       <div className="container around">
+        <div className="menubar" onClick={toggleNav}>
+          <FaBars />
+        </div>
         <div className="brand">
           <NavLink to="/">
-            <h1 className="text-3xl uppercase px-8 py-4 font-bold">Flex Fit</h1>
+            <h1 className="text-3xl uppercase px-8 py-4 font-bold text-center">
+              Flex Fit
+            </h1>
           </NavLink>
         </div>
-        <nav>
-          <div className="container">
+        <nav className={`navbar center ${navActive ? "nav-active" : ""}`}>
+          <div className="container center">
             <ul className="navlist center">
-              <li className="list-item">
+              <li
+                className="list-item"
+                onClick={() => {
+                  setNavActive(false);
+                  scrollToTop();
+                }}
+              >
                 <NavLink to="/" className="navlink p-2 mx-2">
                   Home
                 </NavLink>
               </li>
-              <li className="list-item">
+              <li
+                className="list-item"
+                onClick={() => {
+                  setNavActive(false);
+                  scrollToTop();
+                }}
+              >
                 <NavLink to="/about" className="navlink p-2 mx-2">
                   About
                 </NavLink>
               </li>
-              <li className="list-item">
+              <li
+                className="list-item"
+                onClick={() => {
+                  setNavActive(false);
+                  scrollToTop();
+                }}
+              >
                 <NavLink to="/classes" className="navlink p-2 mx-2">
                   Classes
                 </NavLink>
               </li>
-              <li className="list-item">
+              <li
+                className="list-item"
+                onClick={() => {
+                  setNavActive(false);
+                  scrollToTop();
+                }}
+              >
                 <NavLink to="/trainers" className="navlink p-2 mx-2">
                   Trainers
                 </NavLink>
               </li>
-              <li className="list-item">
+              <li
+                className="list-item"
+                onClick={() => {
+                  setNavActive(false);
+                  scrollToTop();
+                }}
+              >
                 <NavLink to="/contact" className="navlink p-2 mx-2">
                   Contact
                 </NavLink>
